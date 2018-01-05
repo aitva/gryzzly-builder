505890d3308a412e43bb59adbe3aa1a1ab20ecf8
Aitva <arod.louis@gmail.com>
2018-01-05 14:58:11 +0100

    add callback to webhook

---
+++ pkg/webhook/doc.go
@@ -0,0 +1,3 @@
+// Package webhook provide functions to work with Github Webhook.
+// This package is inspired by https://github.com/abiosoft/caddy-git
+package webhook
--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -4,19 +4,46 @@ import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/hex"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// emptyCallback is the default callback.
+var emptyCallback Callback = func(interface{}) error {
+	return nil
+}
+
+// Push represents a push event from Github.
+type Push struct {
+	Ref string `json:"ref"`
+}
+
+// Release represents a release event from Github.
+type Release struct {
+	Action  string `json:"action"`
+	Release struct {
+		TagName string      `json:"tag_name"`
+		Name    interface{} `json:"name"`
+	} `json:"release"`
+}
+
+// Callback represents a callback function for Github event.
+type Callback func(interface{}) error
+
 // Webhook represents a webhook HTTP handler.
 type Webhook struct {
-	l      *log.Logger
-	secret string
+	l               *log.Logger
+	secret          string
+	pushCallback    Callback
+	releaseCallback Callback
 }
 
 type WebhookConfig struct {
-	L *log.Logger
+	L               *log.Logger
+	PushCallback    Callback
+	ReleaseCallback Callback
 }
 
 // New instanciates a new Webhook with the given secret.
@@ -24,9 +51,17 @@ func New(secret string, config WebhookConfig) *Webhook {
 	if config.L == nil {
 		config.L = log.New(ioutil.Discard, "", 0)
 	}
+	if config.PushCallback == nil {
+		config.PushCallback = emptyCallback
+	}
+	if config.ReleaseCallback == nil {
+		config.ReleaseCallback = emptyCallback
+	}
 	return &Webhook{
-		l:      config.L,
-		secret: secret,
+		l:               config.L,
+		secret:          secret,
+		pushCallback:    config.PushCallback,
+		releaseCallback: config.ReleaseCallback,
 	}
 }
 
@@ -60,11 +95,32 @@ func (wh *Webhook) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	event := r.Header.Get("X-Github-Event")
 	switch event {
 	case "ping":
-		w.Write([]byte("pong"))
+		// return with code 200
+		return
 	case "push":
-		// TODO: handle push
+		push := &Push{}
+		err = json.Unmarshal(body, push)
+		if err != nil {
+			wh.l.Println("fail to unmarshal json:", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+		}
+		err = wh.pushCallback(push)
+		if err != nil {
+			wh.l.Println("fail to execute push callback:", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
 	case "release":
-		// TODO: handle release
+		release := &Release{}
+		err = json.Unmarshal(body, release)
+		if err != nil {
+			wh.l.Println("fail to unmarshal json:", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+		}
+		err := wh.releaseCallback(release)
+		if err != nil {
+			wh.l.Println("fail to execute push callback:", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
 	default:
 		// return 400 if we do not handle the event type.
 		// This is to visually show the user a configuration error in the GH ui.
